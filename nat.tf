bc60c347fe29cc7659e8367b345188649d82946e
dhanasreebantu <dhanasreebantu18@gmail.com>
2026-05-23 22:44:49 +0530

    Initial Terraform EKS project

---
+++ nat.tf
@@ -0,0 +1,22 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "main-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
